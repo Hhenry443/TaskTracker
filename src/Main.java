cf80d25ab0a7b7fa28b21f82d3eeed8e1399b2ca
Henry Barnes <135261800+Hhenry443@users.noreply.github.com>
2026-03-08 18:32:22 +0000

    Modify main method to throw exceptions
    Updated main method to handle IOException and ParseException.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+package org.example;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
+        TaskList TL = new TaskList("tasks.json");
 
+        TL.filterTasks("all");
     }
 }
